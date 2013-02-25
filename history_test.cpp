d0616afbe8756924b8f4e23afb0fd03cec085827
Robert Obryk <robryk@gmail.com>
2013-02-25 15:16:05 +0100

    More detailed test for history.
    We test multi-word structures and try to publish different values
    from different threads. This should detect nonatomic behaviour of
    reads and writes to/from buffer and latest.

--- history_test.cpp
+++ history_test.cpp
@@ -4,7 +4,19 @@ const int thread_count = 3;
 const int history_size = 10;
 
 struct history_test : public rl::test_suite<history_test, thread_count> {
-	history<intptr_t> hist;
+	struct element {
+		intptr_t ver;
+		intptr_t tid;
+		intptr_t sum;
+
+		element(intptr_t v, intptr_t t) : ver(v), tid(t), sum(v+t) { }
+		element() = default;
+		bool ok() {
+			return ver + tid == sum && 0 <= tid && tid < thread_count;
+		}
+	};
+	
+	history<element, raw_locked_history> hist;
 	VAR_T(bool) done[2*history_size];
 
 	typedef raw_history::version_t version_t;
@@ -12,10 +24,11 @@ struct history_test : public rl::test_suite<history_test, thread_count> {
 	history_test() : hist(history_size, thread_count) { }
 
 	void check_single_ver(version_t ver) {
-		intptr_t value;
+		element value;
 		bool ok = hist.get(ver, &value);
 		if (ok) {
-			RL_ASSERT(value == ver);
+			RL_ASSERT(value.ok());
+			RL_ASSERT(value.ver == ver);
 		} else {
 			version_t current_ver = hist.get_version();
 			LOG << "current ver " << current_ver;
@@ -30,7 +43,7 @@ struct history_test : public rl::test_suite<history_test, thread_count> {
 
 	void thread(int thread_idx) {
 		for(version_t i=hist.get_version()+1; i<2*history_size; i++) {
-			intptr_t value = i;
+			element value(i, thread_idx);
 			bool ok = hist.publish(thread_idx, i, &value);
 			if (ok) {
 				RL_ASSERT(!VAR(done[i])); // We need not worry about races on done[i] -- they cause the test to fail.
