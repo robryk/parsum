80c32a055af7d573cd5b87982a9667b742cc2591
Robert Obryk <robryk@gmail.com>
2013-02-25 04:53:09 +0100

    Modified history_test to verify return value of publish.

--- history_test.cpp
+++ history_test.cpp
@@ -5,6 +5,7 @@ const int history_size = 10;
 
 struct history_test : public rl::test_suite<history_test, thread_count> {
 	history<intptr_t> hist;
+	VAR_T(bool) done[2*history_size];
 
 	typedef raw_history::version_t version_t;
 
@@ -22,13 +23,27 @@ struct history_test : public rl::test_suite<history_test, thread_count> {
 		}
 	}
 
+	void before() {
+		for(int i=1;i<2*history_size;i++)
+			VAR(done[i]) = false;
+	}
+
 	void thread(int thread_idx) {
 		for(version_t i=hist.get_version()+1; i<2*history_size; i++) {
 			intptr_t value = i;
-			hist.publish(thread_idx, i, &value);
+			bool ok = hist.publish(thread_idx, i, &value);
+			if (ok) {
+				RL_ASSERT(!VAR(done[i])); // We need not worry about races on done[i] -- they cause the test to fail.
+				VAR(done[i]) = true;
+			}
 			LOG << "checking " << i;
 			check_single_ver(i);
 		}
+	}
+
+	void after() {
+		for(int i=1;i<2*history_size;i++)
+			RL_ASSERT(VAR(done[i]));
 	}
 };
 
