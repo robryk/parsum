fe3e2f3c370bd4236825ec6474d5ff412c3410ec
Robert Obryk <robryk@gmail.com>
2013-02-26 11:43:59 +0100

    Refactored parsum into separate classs for nodes and leaves.

--- parsum.hpp
+++ parsum.hpp
@@ -9,48 +9,93 @@ template<typename T> class reducing_snapshot {
 	public:
 		struct update_result {
 			intptr_t update_count;
-			T value_before;
 			intptr_t version;
+			T value_before;
 		};
+
+		constexpr static int INVALID_VERSION = -1;
+
+		virtual intptr_t get_version() = 0;
+		virtual intptr_t get(T* output) = 0;
+		virtual update_result update(intptr_t tid, const T* input) = 0;
+		virtual bool get_last(intptr_t tid, update_result* output) = 0;
+		virtual ~reducing_snapshot() { }
+
+		static reducing_snapshot* create(int thread_count);
+};
+
+typedef raw_locked_history RawHistory;
+typedef raw_locked_holder RawHolder;
+
+template<typename T> class leaf : public reducing_snapshot<T> {
 	private:
-		typedef raw_locked_history RawHistory;
-		typedef raw_locked_holder RawHolder;
-		
+		typedef typename reducing_snapshot<T>::update_result update_result;
+		constexpr static int INVALID_VERSION = reducing_snapshot<T>::INVALID_VERSION;
+
+		history<T, RawHistory> history_;
+	public:
+		virtual intptr_t get_version() {
+			return history_.get_version();
+		}
+
+		virtual intptr_t get(T* output) {
+			intptr_t version = history_.get_version();
+			if (!history_.get(version, output))
+				return INVALID_VERSION;
+			return version;
+		}
+
+		virtual update_result update(intptr_t tid, const T* input) {
+			RL_ASSERT(tid == 0);
+			update_result result;
+			intptr_t version = history_.get_version() + 1;
+			bool ok = history_.publish(0, version, input);
+			RL_ASSERT(ok);
+			ok = get_last(0, &result);
+			RL_ASSERT(ok);
+			return result;
+		}
+
+		virtual bool get_last(intptr_t tid, update_result* output) {
+			RL_ASSERT(tid == 0);
+			intptr_t version = history_.get_version();
+			output->update_count = version;
+			output->version = version;
+			return history_.get(version-1, &output->value_before);
+		}
+
+		leaf() : history_(3, 1) {}
+};
+
+template<typename T> class node : public reducing_snapshot<T> {
+	private:
+		typedef typename reducing_snapshot<T>::update_result update_result;
+		constexpr static int INVALID_VERSION = reducing_snapshot<T>::INVALID_VERSION;
+
 		struct value {
 			T child_values[2];
 			intptr_t child_versions[2];
 		};
-
 		struct latest_value {
 			intptr_t version;
 			T value_before;
 		};
 
 		const int thread_count_;
-		const bool leaf_;
-
-		int child_idx(int tid) {
-			return tid % 2;
-		}
-
-		int child_tid(int tid) {
-			return tid / 2;
-		}
-
-		const string name_;
-
+		
 		history<value, RawHistory> history_;
-		std::unique_ptr<holder<latest_value, RawHolder>[]> latest_; // node only
-		std::unique_ptr<reducing_snapshot> children_[2]; // node only
+		std::unique_ptr<holder<latest_value, RawHolder>[]> latest_;
+		std::unique_ptr<reducing_snapshot<T> > children_[2];
+
+		int child_idx(int tid) { return tid % 2; }
+		int child_tid(int tid) { return tid / 2; }
 
 		void update_last(intptr_t tid) {
-			if (leaf_)
-				return;
 			update_result child_result;
 			bool ok = children_[child_idx(tid)]->get_last(child_tid(tid), &child_result);
 			if (!ok)
 				return;
-			intptr_t version_after = get_version();
+			intptr_t version_after = history_.get_version();
 			intptr_t version_before = version_after - 3*thread_count_;
 			while (version_before + 1 < version_after) {
 				intptr_t middle = (version_before + version_after) / 2;
@@ -75,97 +120,29 @@ template<typename T> class reducing_snapshot {
 				lv.value_before = child_result.value_before + value_before.child_values[1];
 			else
 				lv.value_before = value_after.child_values[0] + child_result.value_before ;
-			LOG << "N" << name_ << " update_last tid=" << tid << " val_before=" << lv.value_before << " version=" << lv.version << " update_count=" << child_result.update_count;
+			//LOG << "N" << name_ << " update_last tid=" << tid << " val_before=" << lv.value_before << " version=" << lv.version << " update_count=" << child_result.update_count;
 			latest_[tid].update(child_result.update_count, &lv);
 		}
-
-		update_result leaf_update(const T* val) {
-			RL_ASSERT(leaf_);
-			update_result res;
-			intptr_t old_version = get(&res.value_before);
-			RL_ASSERT(old_version != INVALID_VERSION);
-			value v;
-			v.child_values[0] = *val;
-			bool ok = history_.publish(0, old_version+1, &v);
-			RL_ASSERT(ok);
-			res.update_count = old_version + 1;
-			res.version = old_version + 1;
-			return res;
-		}
-
-		bool leaf_get_last(update_result* output) {
-			RL_ASSERT(leaf_);
-			intptr_t version = history_.get_version();
-			if (version == 0) {
-				output->update_count = 0;
-				output->version = 0;
-				output->value_before = T();
-				return true;
-			}
-			value buffer;
-			if (!history_.get(version-1, &buffer))
-				return false;
-			output->update_count = version;
-			output->version = version;
-			output->value_before = buffer.child_values[0];
-			return true;
-		}
-
 	public:
-		reducing_snapshot(int thread_count, string name = "") :
-			name_(name),
-			thread_count_(thread_count),
-			leaf_(thread_count == 1),
-			history_(3*thread_count, thread_count),
-			latest_(new holder<latest_value, RawHolder>[thread_count])
-		{
-			if (!leaf_) {
-				children_[0].reset(new reducing_snapshot((thread_count + 1) / 2, name_ + "0"));
-				children_[1].reset(new reducing_snapshot(thread_count / 2, name_ + "1"));
-			}
+		virtual intptr_t get_version() {
+			return history_.get_version();
 		}
 
-		constexpr static int INVALID_VERSION = -1;
-
-		intptr_t get(T* result) {
+		virtual intptr_t get(T* result) {
 			intptr_t version = history_.get_version();
 			value buffer;
 			if (!history_.get(version, &buffer))
 				return INVALID_VERSION;
-			if (leaf_)
-				*result = buffer.child_values[0];
-			else
-				*result = buffer.child_values[0] + buffer.child_values[1];
+			*result = buffer.child_values[0] + buffer.child_values[1];
 			return version;
 		}
 
-		intptr_t get_version() {
-			return history_.get_version();
-		}
-
-		bool get_last(intptr_t tid, update_result* output) {
-			if (leaf_)
-				return leaf_get_last(output);
-			update_last(tid);
-			latest_value last_value;
-			intptr_t last_version = latest_[tid].get(&last_value);
-			if (last_version == holder<latest_value>::INVALID_VERSION)
-				return false;
-			output->update_count = last_version;
-			output->value_before = last_value.value_before;
-			output->version = last_value.version;
-			return true;
-		}
-
-		update_result update(intptr_t tid, const T* val) {
-			if (leaf_)
-				return leaf_update(val);
-
+		virtual update_result update(intptr_t tid, const T* val) {
 			update_result child_result = children_[child_idx(tid)]->update(child_tid(tid), val);
 			intptr_t other_version = children_[1-child_idx(tid)]->get_version();
 
 			do {
-				intptr_t my_version = history_.get_version();
+				intptr_t my_version = get_version();
 				value my_value;
 				if (!history_.get(my_version, &my_value))
 					break;
@@ -178,7 +155,7 @@ template<typename T> class reducing_snapshot {
 					break;
 				update_last(my_version % thread_count_);
 				if (history_.publish(tid, my_version+1, &new_value)) {
-					LOG << "N" << name_ << "succ publish v="<<my_version+1 << " chvers=" << new_value.child_versions[0] << " " << new_value.child_versions[1];
+			//		LOG << "N" << name_ << "succ publish v="<<my_version+1 << " chvers=" << new_value.child_versions[0] << " " << new_value.child_versions[1];
 					break;
 				}
 			} while (true);
@@ -188,8 +165,36 @@ template<typename T> class reducing_snapshot {
 			RL_ASSERT(ok);
 			return result;
 		}
-				
 
+		virtual bool get_last(intptr_t tid, update_result* output) {
+			update_last(tid);
+			latest_value last_value;
+			intptr_t last_version = latest_[tid].get(&last_value);
+			if (last_version == holder<latest_value>::INVALID_VERSION)
+				return false;
+			output->update_count = last_version;
+			output->value_before = last_value.value_before;
+			output->version = last_value.version;
+			return true;
+		}
 
+	
+		node(int thread_count) :
+			thread_count_(thread_count),
+			history_(3*thread_count, thread_count),
+			latest_(new holder<latest_value, RawHolder>[thread_count])
+		{
+			RL_ASSERT(thread_count_ > 1);
+			children_[0].reset(reducing_snapshot<T>::create((thread_count + 1) / 2));
+			children_[1].reset(reducing_snapshot<T>::create(thread_count / 2));
+		}
 };
-			
+
+template<typename T> reducing_snapshot<T>* reducing_snapshot<T>::create(int thread_count) {
+	RL_ASSERT(thread_count > 0);
+	if (thread_count == 1)
+		return new leaf<T>();
+	else
+		return new node<T>(thread_count);
+}
+
--- parsum_test.cpp
+++ parsum_test.cpp
@@ -4,18 +4,18 @@ const int thread_count = 5;
 const int op_count = 2;
 
 struct reducing_snapshot_test : public rl::test_suite<reducing_snapshot_test, thread_count> {
-	reducing_snapshot<intptr_t> summer;
+	std::unique_ptr<reducing_snapshot<intptr_t> > summer;
 	VAR_T(bool) done[thread_count * op_count];
 
 	void thread(int thread_idx) {
 		for(int i=1;i<=op_count;i++) {
 			intptr_t new_value = i;
-			reducing_snapshot<intptr_t>::update_result res = summer.update(thread_idx, &new_value);
+			reducing_snapshot<intptr_t>::update_result res = summer->update(thread_idx, &new_value);
 			VAR(done[res.value_before]) = true;
 		}
 	}
 
-	reducing_snapshot_test() : summer(thread_count) { }
+	reducing_snapshot_test() : summer(reducing_snapshot<intptr_t>::create(thread_count)) { }
 };
 
 
